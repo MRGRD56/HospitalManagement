01b0e58fdc1b57ac6c77417d5694374c44f13360
golikov.kv0467 <bukinist85@gmail.com>
2020-10-08 15:04:44 +0500

    Some changes

---
+++ Golikov_WinFormHospital/PatientForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿using System.ComponentModel;
+
+namespace Golikov_WinFormHospital
+{
+    partial class PatientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FullnameTB = new System.Windows.Forms.TextBox();
+            this.PatientAddressTB = new System.Windows.Forms.TextBox();
+            this.BirthDateTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FullnameTB
+            // 
+            this.FullnameTB.Location = new System.Drawing.Point(121, 12);
+            this.FullnameTB.Name = "FullnameTB";
+            this.FullnameTB.Size = new System.Drawing.Size(244, 20);
+            this.FullnameTB.TabIndex = 15;
+            // 
+            // PatientAddressTB
+            // 
+            this.PatientAddressTB.Location = new System.Drawing.Point(121, 59);
+            this.PatientAddressTB.Name = "PatientAddressTB";
+            this.PatientAddressTB.Size = new System.Drawing.Size(244, 20);
+            this.PatientAddressTB.TabIndex = 17;
+            // 
+            // BirthDateTB
+            // 
+            this.BirthDateTB.Location = new System.Drawing.Point(121, 35);
+            this.BirthDateTB.Name = "BirthDateTB";
+            this.BirthDateTB.Size = new System.Drawing.Size(244, 20);
+            this.BirthDateTB.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "ФИО: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 18);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Дата рождения: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(3, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Адрес пациента: ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // PatientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 131);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FullnameTB);
+            this.Controls.Add(this.PatientAddressTB);
+            this.Controls.Add(this.BirthDateTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "PatientForm";
+            this.Text = "PatientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox PatientAddressTB;
+
+        private System.Windows.Forms.TextBox BirthDateTB;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.TextBox FullnameTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        #endregion
+    }
+}
